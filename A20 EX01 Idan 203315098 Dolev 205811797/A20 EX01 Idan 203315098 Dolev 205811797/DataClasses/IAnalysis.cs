d7342e407004ccecd86390997121930af4f20214
DolevAttiya <50592453+DolevAttiya@users.noreply.github.com>
2019-11-21 23:11:30 +0200

    Added Analytic interface

---
+++ A20 EX01 Idan 203315098 Dolev 205811797/A20 EX01 Idan 203315098 Dolev 205811797/DataClasses/IAnalysis.cs
@@ -0,0 +1,11 @@
+﻿namespace A20_EX01_Idan_203315098_Dolev_205811797.DataClasses
+{
+    public interface IAnalysis
+    {
+        object CalcAnalysis();
+
+        void OrderBy(object i_Param);
+
+        void SynthesizeBy(object i_Param);
+    }
+}
