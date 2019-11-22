6f9b36afc282c632ca8e0057871317dec5b3e176
DolevAttiya <50592453+DolevAttiya@users.noreply.github.com>
2019-11-22 17:17:16 +0200

    Added The Analysis and the Holder of each type per review

---
+++ A20 EX01 Idan 203315098 Dolev 205811797/A20 EX01 Idan 203315098 Dolev 205811797/DataClasses/Analysis.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FacebookWrapper.ObjectModel;
+
+namespace A20_EX01_Idan_203315098_Dolev_205811797.DataClasses
+{
+
+    public abstract class Analysis
+    {
+        public Analysis()
+        {
+            int numberDaysOfWeek = 7;
+            int numberHoursADay = 24;
+            int minutesAndSecond = 0;
+            PhotosArrayList = new AnalysisHolder[numberDaysOfWeek, numberHoursADay];
+            VideosArrayList = new AnalysisHolder[numberDaysOfWeek, numberHoursADay];
+            PostsArrayList = new AnalysisHolder[numberDaysOfWeek, numberHoursADay];
+            CombinedAnalysisHolders = new AnalysisHolder[numberDaysOfWeek, numberHoursADay];
+
+            for(int days = 0; days < numberDaysOfWeek; days++)
+            {
+                for(int hour = 0; hour < numberHoursADay; hour++)
+                {
+
+                    PhotosArrayList[days, hour] = new AnalysisHolder(
+                        new DateTime(2019, 9, days + 1, hour, minutesAndSecond, minutesAndSecond));
+                    VideosArrayList[days, hour] = new AnalysisHolder(
+                        new DateTime(2019, 9, days + 1, hour, minutesAndSecond, minutesAndSecond));
+                    PostsArrayList[days, hour] = new AnalysisHolder(
+                        new DateTime(2019, 9, days + 1, hour, minutesAndSecond, minutesAndSecond));
+                    CombinedAnalysisHolders[days, hour] = new AnalysisHolder(
+                        new DateTime(2019, 9, days + 1, hour, minutesAndSecond, minutesAndSecond));
+                }
+            }
+        }
+
+        public AnalysisHolder[,] PhotosArrayList { get; }
+
+        public AnalysisHolder[,] VideosArrayList { get; }
+
+        public AnalysisHolder[,] PostsArrayList { get; }
+
+        public AnalysisHolder[,] CombinedAnalysisHolders { get; }
+
+        public void AddLikesSchedule(TimerSelector i_Time)
+        {
+
+            /*widrow from DB
+             *For Each photos, videos and photos
+             * += to attribute by time and place
+             * += to combined
+             */
+        }
+    }
+
+    public enum TimerSelector
+    {
+    Day = 7,
+    Month=31,
+    Year=365
+    }
+}
+    
+
+
---
+++ A20 EX01 Idan 203315098 Dolev 205811797/A20 EX01 Idan 203315098 Dolev 205811797/DataClasses/AnalysisHolder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace A20_EX01_Idan_203315098_Dolev_205811797.DataClasses
+{
+    public class AnalysisHolder
+    {
+        private int m_NumberOfLikes;
+
+        private int NumberOfLikes { get; }
+
+        public void AddLikes(int i_NumberOfLikes)
+        {
+            m_NumberOfLikes += i_NumberOfLikes;
+        }
+
+        public DateTime HourlyDateTime { get; set; }
+
+        public AnalysisHolder(DateTime i_HourlyDateTime)
+        {
+            m_NumberOfLikes = 0;
+            HourlyDateTime = i_HourlyDateTime;
+        }
+    }
+}
