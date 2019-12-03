ed1fca94adf04c8db210821498cac3747fc02784
Idan91 <idan.ram91@gmail.com>
2019-12-03 12:23:53 +0200

    Class Diagrams

--- A20 EX01 Idan 203315098 Dolev 205811797/A20 EX01 Idan 203315098 Dolev 205811797/GUI/About.Designer.cs
+++ A20 EX01 Idan 203315098 Dolev 205811797/A20 EX01 Idan 203315098 Dolev 205811797/GUI/About.Designer.cs
@@ -32,7 +32,7 @@ namespace A20_EX01_Idan_203315098_Dolev_205811797.GUI
         private void InitializeComponent()
         {
             this.labelAbout = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.labelContent = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // labelAbout
@@ -40,36 +40,39 @@ namespace A20_EX01_Idan_203315098_Dolev_205811797.GUI
             this.labelAbout.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelAbout.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(52)))), ((int)(((byte)(88)))));
             this.labelAbout.Location = new System.Drawing.Point(0, 0);
+            this.labelAbout.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.labelAbout.Name = "labelAbout";
-            this.labelAbout.Padding = new System.Windows.Forms.Padding(5, 5, 0, 0);
-            this.labelAbout.Size = new System.Drawing.Size(230, 39);
+            this.labelAbout.Padding = new System.Windows.Forms.Padding(10, 10, 0, 0);
+            this.labelAbout.Size = new System.Drawing.Size(460, 75);
             this.labelAbout.TabIndex = 0;
             this.labelAbout.Text = "ABOUT BOOST";
             // 
-            // label1
+            // labelContent
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(52)))), ((int)(((byte)(88)))));
-            this.label1.Location = new System.Drawing.Point(3, 55);
-            this.label1.MaximumSize = new System.Drawing.Size(950, 500);
-            this.label1.Name = "label1";
-            this.label1.Padding = new System.Windows.Forms.Padding(5);
-            this.label1.Size = new System.Drawing.Size(778, 67);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Version: 0.1.0\r\n\r\nBoost for Facebook is a social media management app developed b" +
+            this.labelContent.AutoSize = true;
+            this.labelContent.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelContent.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(52)))), ((int)(((byte)(88)))));
+            this.labelContent.Location = new System.Drawing.Point(6, 106);
+            this.labelContent.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.labelContent.MaximumSize = new System.Drawing.Size(1900, 962);
+            this.labelContent.Name = "labelContent";
+            this.labelContent.Padding = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.labelContent.Size = new System.Drawing.Size(1557, 134);
+            this.labelContent.TabIndex = 1;
+            this.labelContent.Text = "Version: 0.1.0\r\n\r\nBoost for Facebook is a social media management app developed b" +
     "y Idan Ram and Dolev Attiya.";
             // 
             // About
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelContent);
             this.Controls.Add(this.labelAbout);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "About";
-            this.Size = new System.Drawing.Size(1021, 523);
+            this.Size = new System.Drawing.Size(2042, 1006);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -78,6 +81,6 @@ namespace A20_EX01_Idan_203315098_Dolev_205811797.GUI
         #endregion
 
         private System.Windows.Forms.Label labelAbout;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelContent;
     }
 }
