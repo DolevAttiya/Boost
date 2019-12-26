409527bbe5e9019b9e6a6ebaa1439a0ad543420a
DolevAttiya <50592453+DolevAttiya@users.noreply.github.com>
2019-12-26 14:19:22 +0200

    new Enum

---
+++ A20 EX02 IdanRam 203315098 DolevAttiya 205811797/A20 EX02 Idan 203315098 Dolev 205811797/Model/DataClasses/eAnalysisDataBasis.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FacebookWrapper.ObjectModel;
+
+namespace A20_EX02_Idan_203315098_Dolev_205811797.Model.DataClasses
+{
+    public enum eAnalysisDataBasis
+    {
+        Combined = 0,
+        Status = Post.eType.status,
+        Photo = Post.eType.photo,
+        Video = Post.eType.video
+    }
+}
