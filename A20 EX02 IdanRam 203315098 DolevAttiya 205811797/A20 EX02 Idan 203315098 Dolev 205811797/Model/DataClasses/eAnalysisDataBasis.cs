51aa783cc85580ae06fc21fe7a4a0406ad71e07e
Idan91 <idan.ram91@gmail.com>
2020-01-05 17:21:46 +0300

    Update eAnalysisDataBasis.cs

--- A20 EX02 IdanRam 203315098 DolevAttiya 205811797/A20 EX02 Idan 203315098 Dolev 205811797/Model/DataClasses/eAnalysisDataBasis.cs
+++ A20 EX02 IdanRam 203315098 DolevAttiya 205811797/A20 EX02 Idan 203315098 Dolev 205811797/Model/DataClasses/eAnalysisDataBasis.cs
@@ -6,7 +6,7 @@ namespace A20_EX02_Idan_203315098_Dolev_205811797.Model.DataClasses
     {
         Combined = 0,
         Status = Post.eType.status,
-        Photo = Post.eType.photo,
-        Video = Post.eType.video
+        Photos = Post.eType.photo,
+        Videos = Post.eType.video
     }
 }
